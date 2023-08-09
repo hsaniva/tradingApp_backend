11b81dd2db895dd6c4d52a1b9904ea86bddf08ff
AvinashG <avinashgdragonz@gmail.com>
2023-08-09 05:26:35 +0000

    Initial Project setup

---
+++ src/test/java/com/maverick/tradingApp/TradingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maverick.tradingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
