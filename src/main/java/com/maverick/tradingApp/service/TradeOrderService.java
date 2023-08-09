09d3e05550f85decb732034ff15e4e9ba83b3098
kitrak_rev <karthikraja2k1@gmail.com>
2023-08-09 11:44:03 +0000

    Docs added for TradeOrderService

--- src/main/java/com/maverick/tradingApp/service/TradeOrderService.java
+++ src/main/java/com/maverick/tradingApp/service/TradeOrderService.java
@@ -23,36 +23,62 @@ public class TradeOrderService {
     private final OrderRepository orderRepository;
 
     public List<TradeOrderDTO> getAllOrders(){
+        /**
+         * returns list of Trade orders in DTO format
+         */
         return orderRepository.findAll().stream()
                 .map(ObjectConversionHelper::BOToDTO)
                 .toList();
     }
 
     public void deleteTradeOrderById(int tradeOrderId){
+        /**
+         * tradeOrderId:: trade order ID
+         *
+         */
         orderRepository.deleteById(tradeOrderId);
     }
 
     public void createTradeOrder(TradeOrderDTO tradeOrderDTO){
+        /**
+         * tradeOrderDTO:: trade Order DTO
+         */
+
         orderRepository.save(ObjectConversionHelper.DTOToBO(tradeOrderDTO));
     }
 
     public TradeOrderDTO getOrderById(Integer id) {
+        /**
+         * id:: id of the order needed
+         * returns the id selected Trade order DTO
+         */
         TradeOrder tradeOrder = orderRepository.findById(id).get();
         return ObjectConversionHelper.BOToDTO(tradeOrder);
     }
 
     public List<TradeOrderDTO> getOrderByTickerId(String tickerId) {
+        /**
+         * tickerId:: Ticker Id like 'AMZN' or 'AAPL'
+         * returns the list of Trade order DTP filtered by Ticker ID
+         */
         List<TradeOrder> tradeOrders = orderRepository.findByStockTickerLabel(tickerId);
         return tradeOrders.stream().map(ObjectConversionHelper::BOToDTO).toList();
     }
 
     public List<TradeOrderDTO> getOrderByStatusCode(StatusCode statusCode) {
+        /**
+         * statusCode:: Enum can be PENDING
+         * returns the Trade Order DTO filtered by status code
+         */
         List<TradeOrder> tradeOrders = orderRepository.findByStockStatusCode(statusCode);
         return tradeOrders.stream().map(ObjectConversionHelper::BOToDTO).toList();
 
     }
 
     public void updateTradeOrder(TradeOrderDTO tradeOrderDTO) {
+        /**
+         * tradeOrderDTO:: updated TradeOrder DTO
+         */
         orderRepository.save(ObjectConversionHelper.DTOToBO(tradeOrderDTO));
     }
 }
