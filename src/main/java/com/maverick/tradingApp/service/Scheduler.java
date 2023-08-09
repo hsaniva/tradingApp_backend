f81283511c8a792781d4a107f94785c46a39f701
kitrak_rev <karthikraja2k1@gmail.com>
2023-08-09 07:19:45 +0000

    add profile to my functions

--- src/main/java/com/maverick/tradingApp/service/Scheduler.java
+++ src/main/java/com/maverick/tradingApp/service/Scheduler.java
@@ -1,12 +1,14 @@
 package com.maverick.tradingApp.service;
 
 import ch.qos.logback.core.net.SyslogOutputStream;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
 
 @Service
+@Profile("Karthik")
 public class Scheduler {
 
     @Scheduled(fixedRate = 5000)
