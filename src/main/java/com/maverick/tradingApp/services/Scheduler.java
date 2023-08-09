8e33e27569e13c0d108f784fb6b3bd8d5c6098bf
kitrak_rev <karthikraja2k1@gmail.com>
2023-08-09 06:50:46 +0000

    add scheduler example

--- src/main/java/com/maverick/tradingApp/services/Scheduler.java
+++ src/main/java/com/maverick/tradingApp/services/Scheduler.java
@@ -1,2 +1,19 @@
-package com.maverick.tradingApp.services;public class Scheduler {
+package com.maverick.tradingApp.services;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+
+@Service
+public class Scheduler {
+
+    @Scheduled(fixedRate = 5000)
+    public void task1(){
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println("Comleted task1 : "+dtf.format(now));
+    }
+
 }
